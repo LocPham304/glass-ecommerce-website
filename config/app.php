4fc565ef4e438026016130aa1795ccb6ebc199e5
huy0305-26 <tg.huy0305@gmail.com>
2026-05-05 08:15:58 +0700

    chore: fix port dynamic

--- config/app.php
+++ config/app.php
@@ -1,7 +1,16 @@
 <?php
 
+$scheme = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
+$host = $_SERVER['HTTP_HOST'] ?? 'localhost';
+$scriptDirectory = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME'] ?? '/BTL/public/index.php'));
+$scriptDirectory = rtrim($scriptDirectory, '/');
+$projectDirectory = rtrim(str_replace('\\', '/', dirname($scriptDirectory)), '/');
+
+$dynamicBaseUrl = $scheme . '://' . $host . ($scriptDirectory === '' ? '' : $scriptDirectory);
+$dynamicAssetUrl = $scheme . '://' . $host . ($projectDirectory === '' ? '' : $projectDirectory);
+
 return [
     'name' => 'ClearVision',
-    'base_url' => getenv('APP_URL') ?: 'http://localhost/BTL/public',
-    'asset_url' => getenv('ASSET_URL') ?: 'http://localhost/BTL',
+    'base_url' => getenv('APP_URL') ?: $dynamicBaseUrl,
+    'asset_url' => getenv('ASSET_URL') ?: $dynamicAssetUrl,
 ];
